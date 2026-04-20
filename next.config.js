66550e2f60de4f5b37167ea6f7363e61074c61b2
Anish Shaw <anishshaw3136@gmail.com>
2026-04-20 19:25:12 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "quixotic-impala-265.convex.cloud",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "kindred-rhinoceros-563.convex.cloud",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "hushed-dolphin-52.convex.cloud",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+        port: "",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
